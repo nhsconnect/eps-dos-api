c9a3fb326201bb9f79a5bb8df9b9a8bfeb6495dc
Aled Greenhalgh <aled.greenhalgh@nhs.net>
2017-09-26 12:30:20 +0100

    using valid bucket name

--- docker/certbot/push-s3.sh
+++ docker/certbot/push-s3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 cp /etc/letsencrypt/live s3://${ENV}_secrets/letsencrypt/live --recursive --sse
+aws s3 cp /etc/letsencrypt/live s3://${ENV}-secrets/letsencrypt/live --recursive --sse
